7d1d407004290dccd93b2c2443bc196d40cf68d9
Viktor <viktor@farcic.com>
2017-04-23 20:04:17 +0200

    Revert

--- run.sh
+++ run.sh
@@ -1,13 +1,1 @@
-set +e
-
-read PSS < ${PASSWORD_SECRET}
-read USR < ${USER_NAME_SECRET}
-
-set -e
-
-if [ -z "$PSS" ]
-then
-    java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS}
-else
-    java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS} -username $USR -password $PSS
-fi
+java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}-jar-with-dependencies.jar ${COMMAND_OPTIONS}
