faa9b001463c61beea53010bd1b82fcc4d086493
Viktor <viktor@farcic.com>
2017-04-23 19:36:08 +0200

    Backwards compatibility

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+set +e
+
 read PSS < ${PASSWORD_SECRET}
 read USR < ${USER_NAME_SECRET}
-java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS} -username $USR -password $PSS
+
+set -e
+
+if [ -z "$PSS" ]
+then
+    java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS}
+else
+    java -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar ${COMMAND_OPTIONS} -username $USR -password $PSS
+fi
